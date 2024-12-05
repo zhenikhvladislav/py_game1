8beacfbe5bd62dfbf143f4a85112c1cb8ff6b83d
zhenihvladislav <zhenikhvladislav@gmail.com>
2024-12-05 16:10:22 +0300

    задал размеры

---
+++ main.py
@@ -0,0 +1,10 @@
+# Документация: python -m pygame.docs
+
+import pygame
+
+if __name__ == '__main__':
+    pygame.init()
+    size = width, height = 800, 600
+    screen = pygame.display.set_mode(size)
+
+
